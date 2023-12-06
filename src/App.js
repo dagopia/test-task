1d0dd66042bbd600ab9f772a4a9433b167f9bd65
dagopia <tdaguti@gmail.com>
2023-12-06 22:20:01 +0300

    Responssivness Updated.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import './App.css';
+import BitCoin from "./component/BitCoin";
+import "./App.css";
 import React, { useEffect, useState } from 'react';
-import BitCoin from './component/BitCoin';
-
 
 function App() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
   return (
     <div className="App">
-      {isMobile ? (
-        <BitCoin />
-      ) : (
-        <p>This is not a mobile device.</p>
-      )}
+      <BitCoin />
     </div>
   );
 }
